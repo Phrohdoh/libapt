e00987c31355d9140c17fcd5b43b14cda149f364
Taryn Hill <taryn@phrohdoh.com>
2017-01-20 13:40:53 -0600

    Begin implementing SwfHeader parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #[macro_use]
 extern crate enum_primitive;
 
+extern crate byteorder;
+use byteorder::ReadBytesExt;
+
+use std::io::{self, BufRead};
+
 mod actionscript;
 
 pub struct SwfHeader {
@@ -10,4 +15,41 @@ pub struct SwfHeader {
     pub size_twips: (u32, u32),
     pub fps: u16,
     pub num_frames: u16,
+}
+
+#[derive(Debug)]
+pub enum ReadError {
+    StdIoError(io::Error),
+    UnknownFormat(Vec<u8>),
+}
+
+impl From<io::Error> for ReadError {
+    fn from(e: io::Error) -> ReadError {
+        ReadError::StdIoError(e)
+    }
+}
+
+pub fn parse_header(br: &mut BufRead) -> Result<SwfHeader, ReadError> {
+    let sig1 = try!(br.read_u8()); // F => uncompressed, C => compressed
+    let sig2 = try!(br.read_u8());
+    let sig3 = try!(br.read_u8());
+
+    match (sig1, sig2, sig3) {
+        (b'F', b'W', b'S') |
+        (b'C', b'W', b'S') => {}
+        _ => return Err(ReadError::UnknownFormat(vec![sig1, sig2, sig3])),
+    }
+
+    let version = try!(br.read_u8());
+
+    Ok(SwfHeader {
+        is_compressed: sig1 == b'C',
+        version: version,
+
+        // TODO
+        len: 0,
+        size_twips: (0, 0),
+        fps: 0,
+        num_frames: 0,
+    })
 }
--- tools/sageapt/src/main.rs
+++ tools/sageapt/src/main.rs
@@ -1,5 +1,46 @@
+use std::fs::File;
+use std::io::BufReader;
+
+extern crate clap;
+use clap::{Arg, App, AppSettings, SubCommand};
+
 extern crate libapt;
+use libapt::{SwfHeader, ReadError};
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("sageapt")
+        .version("0.1.0")
+        .author("Taryn Hill <taryn@phrohdoh.com>")
+        .about("CLI for libapt")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(SubCommand::with_name("parse-header")
+            .about("Parse the header of an APT file")
+            .version("0.1.0")
+            .author("Taryn Hill <taryn@phrohdoh.com>")
+            .arg(Arg::with_name("apt_path")
+                .value_name("apt_path")
+                .required(true)
+                .index(1)))
+        .get_matches();
+
+    let res: Result<SwfHeader, ReadError> = match matches.subcommand() {
+        ("parse-header", Some(args)) => cmd_parse_header(args),
+        _ => unreachable!(),
+    };
+
+    let code = if let Some(e) = res.err() {
+        println!("CliError: {:?}", e);
+        255
+    } else {
+        0
+    };
+
+    std::process::exit(code);
+}
+
+fn cmd_parse_header(args: &clap::ArgMatches) -> Result<SwfHeader, ReadError> {
+    let path = args.value_of("apt_path").unwrap();
+    let f = try!(File::open(&path));
+    let mut br = BufReader::new(f);
+    libapt::parse_header(&mut br)
 }
